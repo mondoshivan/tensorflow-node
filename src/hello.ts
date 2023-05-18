84238015ab1f1920664a89658d37bb4b45dcc1c2
Oliver Schmidt <o.schmidt@steinberg.de>
2023-05-18 10:56:52 +0200

    [remove] dummy files

--- src/hello.ts
+++
@@ -1,8 +0,0 @@
-
-export class Hello {
-
-  static say() {
-    return 'Hello';
-  }
-
-}
--- test/hello.test.ts
+++
@@ -1,8 +0,0 @@
-import { Hello } from '../src/hello';
-
-describe('Hello', function () {
-  it('should GET / with 200 OK', function () {
-    
-    expect(Hello.say()).toEqual('Hello');
-  });
-});
